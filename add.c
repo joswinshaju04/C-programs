730f0f74eafdef2df37857a544bb80e3bfd70872
JOSWIN SHAJU <joswinshaju2028@cs.sjcetpalai.ac.in>
2025-02-03 23:43:21 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int num1,num2,sum;
+        printf("\nenter the no:");
+        scanf("%d",&num1);
+        printf("\nenter the no:");
+        scanf("%d",&num2);
+        sum=num1+num2;
+        
+        printf("\nThe sum of %d and %d is : %d",num1,num2,sum);
+    return 0 ;
+}
