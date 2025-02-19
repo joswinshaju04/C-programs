bc2d8fbe66586d255b034a012f4f24e9dc1ef2d8
JOSWIN SHAJU <joswinshaju2028@cs.sjcetpalai.ac.in>
2025-02-19 14:52:56 +0530

    Add files via upload

---
+++ str_compare.c
@@ -0,0 +1,22 @@
+/*Author:Joswin Shaju
+  Date:19-02-2025
+  a program to input two strings and compare them.
+*/
+
+#include<string.h>
+#include<stdio.h>
+int main(){
+  char str1[20],str2[20];
+  printf("\n Enter the first string:");
+  scanf("%s",str1);
+  printf("\n Enter the first string:");
+  scanf("%s",str2);
+  
+  int comp=strcmp(str1,str2);
+  if(comp==0){
+    printf("\n Its equal");
+  }else{
+    printf("\n Its not equal");
+  }
+  return 0;
+}
